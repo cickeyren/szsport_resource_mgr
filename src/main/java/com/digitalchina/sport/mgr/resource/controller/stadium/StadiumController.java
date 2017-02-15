922c1c7a4b2861db2fb0a7f8a03581133cb87e1f
yangyt <1003162365@qq.com>
2017-02-15 10:30:53 +0800

    添加场馆资源结构

---
+++ src/main/java/com/digitalchina/sport/mgr/resource/controller/stadium/StadiumController.java
@@ -0,0 +1,9 @@
+package com.digitalchina.sport.mgr.resource.controller.stadium;
+
+/**
+ * @Author:yangyt
+ * @Description
+ * @Date:Created in 10:30 2017/2/15
+ */
+public class StadiumController {
+}
